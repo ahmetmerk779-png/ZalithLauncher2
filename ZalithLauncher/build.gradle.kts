f57eb3d2afb14b524f14a7038a9c10ce2f9a7be3
Dkdkdkdk <ahmetmerk779@gmail.com>
2026-08-29 07:23:17 +0300

    Update build.gradle.kts

--- ZalithLauncher/build.gradle.kts
+++ ZalithLauncher/build.gradle.kts
@@ -73,7 +73,7 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            signingConfig = signingConfigs.getByName("releaseBuild")
+            signingConfig = signingConfigs.getByName("debugBuild")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
